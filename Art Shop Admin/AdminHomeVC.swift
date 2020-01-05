fdd17602b95c5e584cfb5708542bb56330f0e09f
Maxim <55534590+Glomis@users.noreply.github.com>
2020-01-05 23:10:34 +0300

    Исправил AdminHomeVC

--- Art Shop Admin/AdminHomeVC.swift
+++ Art Shop Admin/AdminHomeVC.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AdminHomeVC: HomeVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        navigationItem.leftBarButtonItem?.isEnabled = false
     }
 
 
